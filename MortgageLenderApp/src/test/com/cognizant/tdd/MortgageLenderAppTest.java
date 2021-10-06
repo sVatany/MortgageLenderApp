2d3fe843f60fa573b6f015b48efed16edcc7469a
rmj100 <rmj24@njit.edu>
2021-10-06 12:14:03 -0400

    added test case
    added Junit5 and a simple test case that will fail

--- MortgageLenderApp/src/test/com/cognizant/tdd/MortgageLenderAppTest.java
+++ MortgageLenderApp/src/test/com/cognizant/tdd/MortgageLenderAppTest.java
@@ -1,5 +1,21 @@
 package test.com.cognizant.tdd;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+
 public class MortgageLenderAppTest {
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		
+	}
+	
+	@Test
+	void testCheckLenderBalance() {
+		fail("this will fail");
+	}
 
 }
